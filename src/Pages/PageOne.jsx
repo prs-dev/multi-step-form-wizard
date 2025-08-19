d17c508f3331febdc0fa8b0d9fc76931d4ccbe95
prs-dev <pratyushsri.25@gmail.com>
2025-08-19 22:49:57 +0530

    added reducer for validation

--- src/Pages/PageOne.jsx
+++ src/Pages/PageOne.jsx
@@ -1,13 +1,58 @@
-import {useState} from 'react'
+import { useState, useReducer } from 'react'
 import { useStore } from "../store/store"
 
+const initialState = {
+    name: {
+        value: '',
+        touched: '',
+        error: null
+    },
+    email: {
+        value: '',
+        touched: '',
+        error: null
+    },
+    phone: {
+        value: '',
+        touched: '',
+        error: null
+    }
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'name':
+            return { ...state, name: {
+                value: action.payload.value,
+                touched: true,
+                error: action.payload.error
+            } }
+        case 'email':
+            return { ...state, email: {
+                value: action.payload.value,
+                touched: true,
+                error: action.payload.error
+            } }
+        case 'phone':
+            return { ...state, phone: {
+                value: action.payload.value,
+                touched: true,
+                error: action.payload.error
+            } }
+        default:
+            throw new Error(`Unknown action type: ${action.type}`)
+    }
+}
+
 const PageOne = () => {
-    const {page, changePage, setData} = useStore()
-    const [value, setValue] = useState({
-        name: '',
-        email: "",
-        phone: ''
-    })
+    const { page, changePage, setData } = useStore()
+    const [state, dispatch] = useReducer(reducer, initialState)
+    // const [value, setValue] = useState({
+    //     name: '',
+    //     email: "",
+    //     phone: ''
+    // })
+
     // console.log(changePage, page)
     const pageStyle = {
         width: "70%",
@@ -38,26 +83,52 @@ const PageOne = () => {
     const labelStyles = {
         fontSize: "20px"
     }
-    const handleChange = e => {
-        setValue(prev => ({
-            ...prev,
-            [e.target.name]: e.target.value
-        }))
-    }
-    console.log("test", value)
+    // const handleChange = e => {
+    //     setValue(prev => ({
+    //         ...prev,
+    //         [e.target.name]: e.target.value
+    //     }))
+    // }
+    console.log("test", state)
     return (
         <div style={pageStyle}>
             <div style={containerStyles}>
                 <label style={labelStyles} htmlFor="">Name</label>
-                <input type="text" name="name" style={inputStyles} value={value.name} onChange={handleChange}/>
+                <input type="text" name="name"
+                    className={state.name.error ? 'error' : ''}
+                    style={inputStyles}
+                    // value={value.name}
+                    value={state.name.value}
+                    // onChange={handleChange}
+                    onChange={e => dispatch({ type: 'name', payload: {
+                        value: e.target.value,
+                        error: state.name.touched ? e.target.value.length === 0 : null
+                    }})}
+                />
             </div>
             <div style={containerStyles}>
                 <label style={labelStyles} htmlFor="">Email</label>
-                <input type="email" name="email" style={inputStyles} value={value.email} onChange={handleChange}/>
+                <input type="email" name="email"
+                    style={inputStyles}
+                    className={state.email.error ? 'error' : ''}
+                    value={state.email.value}
+                    onChange={e => dispatch({ type: 'email', payload: {
+                        value: e.target.value,
+                        error: state.email.touched ? e.target.value.length === 0 : null
+                    }})}
+                />
             </div>
             <div style={containerStyles}>
                 <label style={labelStyles} htmlFor="">Phone</label>
-                <input type="number" name="phone" style={inputStyles} value={value.phone} onChange={handleChange}/>
+                <input type="number" name="phone"
+                    style={inputStyles}
+                    className={state.phone.error ? 'error' : ''}
+                    value={state.phone.value}
+                    onChange={e => dispatch({ type: 'phone', payload: {
+                        value: e.target.value,
+                        error: state.phone.touched ? e.target.value.length === 0 : null
+                    }})}
+                />
             </div>
             <div>
                 <button onClick={() => {
